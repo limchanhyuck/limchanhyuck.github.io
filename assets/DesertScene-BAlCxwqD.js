51ed00329731b59dfc466af876a98a9eef098477
ImChanHyuck <ibb61@naver.com>
2026-02-12 11:48:40 +0900

    Updates

--- assets/DesertScene-BAlCxwqD.js
+++ assets/DesertScene-BAlCxwqD.js
@@ -1,4 +1,4 @@
-import{r as He,a as dx,g as px,j as Ut}from"./index-D1nXeKga.js";/**
+import{r as He,a as dx,g as px,j as Ut}from"./index-DvSE97XS.js";/**
  * @license
  * Copyright 2010-2024 Three.js Authors
  * SPDX-License-Identifier: MIT
